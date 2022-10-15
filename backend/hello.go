f85f16631bf73ad96349f41b19b81efa3cc5e2be
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2022-10-15 15:50:04 +0800

    get history img for user

--- backend/hello.go
+++ backend/hello.go
@@ -31,7 +31,7 @@ func initDB() (err error) {
 	return nil
 }
 
-type Img struct {
+type ImgInfo struct {
 	Filename string
 	UserId   int
 }
@@ -50,7 +50,6 @@ type ProductInfo struct {
 }
 
 func queryUserRowByName(searchName any) UserInfo {
-	// 该查询语句等价于select id,name,phone from user where id = 1;
 	var id int
 	var username string
 	var pw string
@@ -67,7 +66,6 @@ func queryUserRowByName(searchName any) UserInfo {
 }
 
 func queryProductRowByName(searchCat any) ProductInfo {
-	// 该查询语句等价于select id,name,phone from user where id = 1;
 	var description string
 	var category string
 	var price float64
@@ -80,6 +78,27 @@ func queryProductRowByName(searchCat any) ProductInfo {
 	fmt.Println("query success!")
 	fmt.Printf("category: %s, description: %s, price: %f\n", category, description, price)
 	return productInfo
+}
+
+func queryImgRowByUserId(userId int) []string {
+	var filename string
+	var files []string
+	rows, err := db.Query("select filename from img_res where userid=?", userId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(&filename); err != nil {
+			panic(err)
+		}
+		files = append(files, filename)
+	}
+
+	fmt.Println("query success!")
+	return files
 }
 
 func insertUserRow(id int, username string, password string, userid int) {
@@ -199,44 +218,16 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func getFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
-	// Parse our multipart form, 10 << 20 specifies a maximum
-	// upload of 10 MB files.
-	r.ParseMultipartForm(10 << 20)
-	// FormFile returns the first file for the given key `myFile`
-	// it also returns the FileHeader so we can get the Filename,
-	// the Header and the size of the file
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
-	}
-
-	defer file.Close()
-
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("data", "upload-*.png")
+	userid := r.FormValue("userid")
+	intUserid, err := strconv.Atoi(userid)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer tempFile.Close()
-
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
+	files := queryImgRowByUserId(intUserid)
+	for i := 0; i < len(files); i++ {
+		fmt.Println(files[i])
 	}
-	// write this byte array to our temporary file
-	tempFile.Write(fileBytes)
-	// return that we have successfully uploaded our file!
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
 }
 
 func searchUserByName(w http.ResponseWriter, r *http.Request) {
