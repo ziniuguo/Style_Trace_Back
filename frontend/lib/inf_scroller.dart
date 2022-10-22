a56dd6b242bcb93bb21dd418131263eb5654d095
Ziniu <guo.ziniu.1003@gmail.com>
2022-10-22 15:25:49 +0800

    feat: chore: implemented back2top btn and clear structure

--- frontend/lib/inf_scroller.dart
+++ frontend/lib/inf_scroller.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'controller.dart';
@@ -13,46 +14,35 @@ void main() {
 
 class InfScroller extends StatelessWidget {
   const InfScroller({super.key});
+
   @override
   Widget build(BuildContext context) {
-    RandomWords randomWords = const RandomWords();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to Flutter'),
-          ),
-          body: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      child: const Text('To Top'),
-                      onPressed: () async {
-                        // Delay to make sure the frames are rendered properly
-                        await Future.delayed(const Duration(milliseconds: 300));
-                        SchedulerBinding.instance.addPostFrameCallback((_) {
-                          Controller.controller.animateTo(
-                              Controller.controller.position.minScrollExtent,
-                              duration: const Duration(milliseconds: 400),
-                              curve: Curves.fastOutSlowIn);
-                        });
-                      }),
-                  ElevatedButton(
-                      child: const Text('To Bottom'),
-                      onPressed: () {
-
-                      }),
-                ],
-              ),
-              Expanded(
-                child: Center(
-                  child: randomWords,
-                )
-              )
-            ],
-          )),
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(child: RandomWords()),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+debugL("scrolling!");
+            SchedulerBinding.instance.addPostFrameCallback((_) {
+              Controller.controller.animateTo(
+                  Controller.controller.position.minScrollExtent,
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.fastOutSlowIn);
+            });
+          },
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.navigation),
+        ),
+      ),
     );
+  }
+}
+void debugL(Object o)  {
+  if (kDebugMode) {
+    print("inf_scroller.dart: - $o");
   }
 }
---
+++ frontend/lib/running_widget.dart
@@ -0,0 +1,7 @@
+import 'img_scanner.dart';
+import 'inf_scroller.dart';
+
+class RunningWidget {
+  static const InfScroller infScroller = InfScroller();
+  static const ImgScanner imgScanner = ImgScanner();
+}
